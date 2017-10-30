3e5589f3e1d362c0013f16a9efd7a44a7967abe3
palmerg <palmerg@PALMERG-E7440>
2017-10-30 12:01:01 -0400

    Add project files.

---
+++ siplog/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace siplog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
